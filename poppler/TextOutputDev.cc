7e0b82d92fcfeb17f3a2f29c459e7e310fb9ec3a
Steven Maude <git@stevenmaude.co.uk>
2026-03-16 17:09:20 +0000

    Revert "[TextOutputDev] Use word center point for sorting text lines"
    This reverts commit 8109826642bbc7080150fb18a43de36033b7995b.

--- poppler/TextOutputDev.cc
+++ poppler/TextOutputDev.cc
@@ -915,19 +915,7 @@ void TextPool::sort()
         return back;
     };
 
-    const auto PosCmp = [](const TextWord &a, const TextWord &b) -> bool {
-        if (a.rot == 0) {
-            return ((a.xMin + a.xMax) <= (b.xMin + b.xMax));
-        } else if (a.rot == 1) {
-            return ((a.yMin + a.yMax) <= (b.yMin + b.yMax));
-        } else if (a.rot == 2) {
-            return ((b.xMin + b.xMax) <= (a.xMin + a.xMax));
-        } else {
-            return ((b.yMin + b.yMax) <= (a.yMin + a.yMax));
-        }
-    };
-
-    const auto SortedMerge = [PosCmp](TextWord *a, TextWord *b) {
+    const auto SortedMerge = [](TextWord *a, TextWord *b) {
         if (!a) {
             return b;
         } else if (!b) {
@@ -937,7 +925,7 @@ void TextPool::sort()
         TextWord *head = nullptr;
         TextWord *cursor = nullptr;
 
-        if (PosCmp(*a, *b)) {
+        if (a->primaryCmp(b) <= 0) {
             head = cursor = a;
             a = a->next;
         } else {
@@ -946,7 +934,7 @@ void TextPool::sort()
         }
 
         while (a && b) {
-            if (PosCmp(*a, *b)) {
+            if (a->primaryCmp(b) <= 0) {
                 cursor->next = a;
                 cursor = cursor->next;
                 a = a->next;
